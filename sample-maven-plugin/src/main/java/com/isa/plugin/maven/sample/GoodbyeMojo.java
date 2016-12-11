6579d05159c294c032cb2e975769fd676e3c2f15
isaolmez <isaolmez@gmail.com>
2016-12-11 23:53:44 +0200

    initial commit

---
+++ sample-maven-plugin/src/main/java/com/isa/plugin/maven/sample/GoodbyeMojo.java
@@ -0,0 +1,31 @@
+package com.isa.plugin.maven.sample;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Basic maven plugin example that showcases parameter usage and default phase binding 
+ */
+
+@Mojo(name = "goodbye", defaultPhase = LifecyclePhase.COMPILE)
+public class GoodbyeMojo extends AbstractMojo {
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+	@Parameter(defaultValue = "${project.artifactId}")
+	private String project;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		Date now = new Date();
+		getLog().info(dateFormat.format(now));
+		getLog().info(project + " =====> Goodbye world!");
+
+	}
+}
---
+++ sample-maven-plugin/src/main/java/com/isa/plugin/maven/sample/GreetingMojo.java
@@ -0,0 +1,32 @@
+package com.isa.plugin.maven.sample;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+/*
+ * Basic maven plugin example that shows a parameterless plugin
+ */
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Basic maven plugin example that showcases parameter usage 
+ */
+
+@Mojo(name = "hello")
+public class GreetingMojo extends AbstractMojo {
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+	@Parameter(defaultValue = "${project.artifactId}")
+	private String project;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		Date now = new Date();
+		getLog().info(dateFormat.format(now));
+		getLog().info(project + " =====> Hello world!");
+	}
+}
