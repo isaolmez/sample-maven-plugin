b6bf1baf847908166d961c96e2b01e6bc988bec3
isaolmez <isaolmez@gmail.com>
2017-04-28 11:47:37 +0300

    remove forgotten comment

--- sample-maven-plugin/src/main/java/com/isa/maven/plugin/sample1/GreetingMojo.java
+++ sample-maven-plugin/src/main/java/com/isa/maven/plugin/sample1/GreetingMojo.java
@@ -6,9 +6,6 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-/*
- * Basic maven plugin example that shows a parameterless plugin
- */
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
